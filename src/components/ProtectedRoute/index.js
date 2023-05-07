62997325309084c031146fcfcab1144d14e0a4ae
Sameer2297 <sameerguptabandaru@gmail.com>
2023-05-07 19:26:51 +0000

    Authentication Functionality

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,12 @@
+import {Route, Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const jwt = Cookies.get('isUserLogged')
+  if (jwt === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
